c2933b65ac305e09a4051a5f4ba691cff805ef35
DissRadish <radishalsudais07@gmail.com>
2026-05-15 13:13:25 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
-# Import semua kelas yang dibutuhkan
 from laptop import Laptop
 from smartphone import Smartphone
+from tablet import Tablet  # Import kelas baru
 
 def cetak_label_inventaris(item):
-    """Contoh Subtyping: menerima objek apa pun yang merupakan turunan Produk"""
+    """
+    Inilah kekuatan SUBTYPING. 
+    Meskipun kita tambah kelas baru (Tablet), fungsi ini 
+    TIDAK PERLU diubah sama sekali karena Tablet adalah subtype dari Produk.
+    """
     print(f"Mencetak Label: {item.deskripsi()}")
 
 if __name__ == "__main__":
+    # Inisialisasi berbagai macam objek
     laptop_pro = Laptop("Asus ROG", 18000000, 16)
     phone_pro = Smartphone("Samsung S24", 14000000, 50)
+    tablet_pro = Tablet("iPad Air", 10500000, 10.9) # Objek baru
 
-    print("=== SISTEM INVENTARIS TERPISAH ===\n")
+    print("=== SISTEM INVENTARIS TOKO (Update) ===\n")
+    
+    # Semua objek ini diperlakukan sebagai 'Produk' (Subtyping)
     cetak_label_inventaris(laptop_pro)
     cetak_label_inventaris(phone_pro)
+    cetak_label_inventaris(tablet_pro)
